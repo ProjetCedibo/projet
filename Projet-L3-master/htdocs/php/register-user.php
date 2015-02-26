60a6960d9fe5f17b4c3c3f86ce70df7b48f66f30
Erolyam <obibim@hotmail.fr>
2015-02-26 01:28:29 +0100

    Bon site bordel :)

---
+++ Projet-L3-master/htdocs/php/register-user.php
@@ -0,0 +1,38 @@
+<?php 
+
+	include './bibli_bd.php';
+
+
+	ob_start();
+
+	isset($_POST['DeviceID']) ? addUser() : null; 
+
+	ob_end_flush();
+
+	function addUser(){
+		date_default_timezone_set("Europe/Paris");
+		$DeviceID = $_POST['DeviceID'];
+		$DeviceModel = $_POST['DeviceModel'];
+		$DeviceOS = $_POST['DeviceOS'];
+	    bd_Connecter();
+
+	    $sql= "SELECT UserId FROM User WHERE UserDevice='$DeviceID'";
+		$r = mysql_query($sql);
+		$res = mysql_fetch_row($r);
+		if ($res <= 0){
+			$sql=mysql_query("INSERT INTO User (UserDevice, UserModel, UserOs, UserLogin) VALUES ('$DeviceID','$DeviceModel', '$DeviceOS',0)");
+			$UserId = mysql_insert_id();
+		}else{
+			$UserId = $res[0];
+		}
+		$date = date("Y-m-d");
+		$heure = date("H:i:s");
+		$sql = "INSERT INTO Connection (ConnectionDate, ConnectionHour, ConnectionUser) VALUES ('$date','$heure','$UserId')";
+		$r = mysql_query($sql);
+	    
+	    mysql_close();
+
+	}
+
+
+?>
